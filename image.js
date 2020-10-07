21e77a911d9ab0042ad2882e5264224c2aa79ecb
srolisp <69720792+srolisp@users.noreply.github.com>
2020-10-07 19:52:42 +0900

    Add files via upload

---
+++ image.js
@@ -0,0 +1,33 @@
+function readURL(input, ts) {
+    if (input.files && input.files[0]) {
+
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.image-upload-wrap').hide();
+
+            $('.file-upload-image').attr('src', e.target.result);
+            $('.file-upload-content').show();
+
+            $('.image-title').html(input.files[0].name);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+	// init 함수를 여기서 실행하지 말고.. init 함수를 인자로 받아서 실행 할 수 있게 하자
+        // init() 			// tmachine.js
+    } else {
+        removeUpload();
+    }
+}
+
+function removeUpload() {
+    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+    $('.file-upload-content').hide();
+    $('.image-upload-wrap').show();
+}
+$('.image-upload-wrap').bind('dragover', function () {
+    $('.image-upload-wrap').addClass('image-dropping');
+});
+$('.image-upload-wrap').bind('dragleave', function () {
+    $('.image-upload-wrap').removeClass('image-dropping');
+});
